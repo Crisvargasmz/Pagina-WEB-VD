db2a57d83606d53da2fef812c3365fdaf608551b
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2023-10-22 23:15:49 -0600

    En app se agrego las importaciones y en index lo de librerias para iconos

--- frontend-vd/src/App.js
+++ frontend-vd/src/App.js
@@ -4,6 +4,14 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Producto from './pages/Producto';
 import Gestionproducto from './pages/Gestionproducto';
+import Categoria from './pages/Categoria';
+import Marca from './pages/Marca';
+import Gestioncategoria from './pages/Gestioncategoria';
+import Gestionmarca from './pages/Gestionmarca';
+import Usuario from './pages/Usuario';
+import Gestionusuario from './pages/Gestionusuario';
+import Comentario from './pages/Comentario';
+import Gestioncomentario from './pages/Gestioncomentario';
 
 function App() {
   return (
@@ -13,6 +21,14 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/Producto" element={<Producto />} />
         <Route path="/Gestionproducto" element={<Gestionproducto />} />
+        <Route path="/Categoria" element={<Categoria/>} />
+        <Route path="/Marca" element={<Marca/>} />
+        <Route path="/Gestioncategoria" element={<Gestioncategoria/>} />
+        <Route path="/Gestionmarca" element={<Gestionmarca/>} />
+        <Route path="/Usuario" element={<Usuario/>} />
+        <Route path="/Gestionusuario" element={<Gestionusuario/>} />
+        <Route path="/Comentario" element={<Comentario/>} />
+        <Route path="/Gestioncomentario" element={<Gestioncomentario/>} />
       </Routes>
     </Router>
   );
--- frontend-vd/src/index.js
+++ frontend-vd/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+library.add(solidStar, regularStar);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
