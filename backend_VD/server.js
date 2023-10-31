f4308876d3a1ac2947f67b22a3feee51e2b904ae
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2023-10-30 22:30:39 -0600

    Se agrego import para poder subir imagenes y despues convertilo en url y se guarde en el servidor

--- backend_VD/server.js
+++ backend_VD/server.js
@@ -1,36 +1,59 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
+const multer = require('multer'); 
+const path = require('path');
 const app = express();
 const port = 5000;
+
+
 app.use(express.json());
 
 const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'Devloper',
+  password: 'dev2023',
+  database: 'variedadesduarte',
+});
 
-    host: 'localhost',
-    user: 'Devloper',
-    password: 'dev2023',
-    database: 'variedadesduarte'
-
-    });
-
-    db.connect((err) => {
-
-    if (err) {
-
+db.connect((err) => {
+  if (err) {
     console.error('Error de conexión a la base de datos:', err);
-} else {
-    console.log('Conexión exitosa a la base de datos' );
-    }
+  } else {
+    console.log('Conexión exitosa a la base de datos');
+  }
 });
 
 app.use(cors());
 
-    // Configuración de CORS
-    app.listen(port, () => {
-        console.log(`Servidor backend funcionando en el puerto:${port}`);
+// Configura el almacenamiento para los archivos subidos
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
+    },
+    filename: function (req, file, cb) {
+      const extension = path.extname(file.originalname);
+      cb(null, Date.now() + extension); // Nombre del archivo se genera con una marca de tiempo para evitar conflictos
+    },
+  });
 
+// Crea una instancia de multer con la configuración de almacenamiento
+const upload = multer({ storage: storage });
+
+// Maneja la carga de archivos
+app.post('/upload', upload.single('imagen'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).send('No se ha seleccionado ningún archivo.');
+    }
+    const imageUrl = 'http://localhost:5000/' + req.file.path;
+    res.json({ imageUrl });
+  });
+
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Configuración de CORS
+app.listen(port, () => {
+  console.log(`Servidor backend funcionando en el puerto:${port}`);
 });
 
 app.use(cors());
