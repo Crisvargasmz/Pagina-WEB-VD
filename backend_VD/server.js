d7381682be587a2f4f5c780b61d90e331146ab8b
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2023-11-03 16:45:45 -0600

    Se agrego otra ruta para actualizacion de imagen

--- backend_VD/server.js
+++ backend_VD/server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-
 app.use(express.json());
 
 const db = mysql.createConnection({
@@ -28,28 +27,38 @@ app.use(cors());
 
 // Configura el almacenamiento para los archivos subidos
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
-    },
-    filename: function (req, file, cb) {
-      const extension = path.extname(file.originalname);
-      cb(null, Date.now() + extension); // Nombre del archivo se genera con una marca de tiempo para evitar conflictos
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname);
+    cb(null, Date.now() + extension); // Nombre del archivo se genera con una marca de tiempo para evitar conflictos
+  },
+});
 
 // Crea una instancia de multer con la configuración de almacenamiento
 const upload = multer({ storage: storage });
 
 // Maneja la carga de archivos
 app.post('/upload', upload.single('imagen'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).send('No se ha seleccionado ningún archivo.');
-    }
-    const imageUrl = 'http://localhost:5000/' + req.file.path;
-    res.json({ imageUrl });
-  });
+  if (!req.file) {
+    return res.status(400).send('No se ha seleccionado ningún archivo.');
+  }
+  const imageUrl = 'http://localhost:5000/' + req.file.path;
+  res.json({ imageUrl });
+});
 
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.post('/upload2', upload.single('nuevaImagen'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No se ha seleccionado ningún archivo.');
+  }
+  res.json({ imageUrl: 'http://localhost:5000/' + req.file.path });
+});
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
+  maxAge: 0, // No almacenar en caché (0 segundos)
+}));
 
 // Configuración de CORS
 app.listen(port, () => {
